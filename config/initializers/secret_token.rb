ebee825f5f10dea2a0823a9dc14c2faf5e8bda09
Nícolas Iensen <nicolas@engage.is>
2013-10-11 19:03:35 -0300

    [#58504776] rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etail::Application.config.secret_key_base = '22416f2513902fee53e499067fc8162ce621ff456c35cffa68996f221d55ab9b04f9bb79bc5c4ce78d526be93da9764955ad2c821ef0e6629ffdda1fcbc19161'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etail::Application.config.session_store :cookie_store, key: '_etail_session'

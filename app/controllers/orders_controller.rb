af945f3a1cfbe1776086bece634c0d1257923e72
Nícolas Iensen <nicolas@iensen.me>
2013-10-12 15:34:11 -0300

    [#58757756] add skip_before_filter :verify_authenticity_token to orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def confirm
     order = Order.find_by_uid(params[:uid])
     order.update_attributes confirmed_at: Time.now

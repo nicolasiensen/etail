88e054439d552743585492eb4c3977df04e4a118
Nícolas Iensen <nicolas@iensen.me>
2013-11-25 16:18:52 -0200

    [#58504776] Fix revenue CSV

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ActiveRecord::Base
     confirmed_at.present?
   end
 
-  def revenue
+  def self.revenue
     execute("SELECT to_char(orders.created_at, 'yyyymmdd') AS \"day\", sum(orders.subtotal) AS \"subtotal\" FROM orders GROUP BY 1 ORDER BY 1 DESC")
   end
 end
